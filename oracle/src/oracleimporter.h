a585bf0f000a9d2dee4c1cd7ed0a6b46e5f09962
Stolas <610753+Stolas@users.noreply.github.com>
2024-02-04 15:41:11 +0100

    Schrecknet Oracle; Ordering the sets, and fix some of the short names.

--- oracle/src/oracleimporter.h
+++ oracle/src/oracleimporter.h
@@ -48,50 +48,53 @@ public:
           releaseDate(_releaseDate), setType(std::move(_setType))
     {
         /* Schrecknet: To whomever it might concern, yes this is most def not the best way to implement this. */
-        if (longName == "Shadows of Berlin") { shortName = QString("SoB"); return; }
-        if (longName == "Danse Macabre") { shortName = QString("DM");  return; }
-        if (longName == "Anarchs") { shortName = QString("A"); return; }
-        if (longName == "Sabbat War") { shortName = QString("SW"); return; }
-        if (longName == "New Blood") { shortName = QString("NB"); return; }
-        if (longName == "Gehenna") { shortName = QString("G"); return; }
-        if (longName == "Fall of London") { shortName = QString("FoL"); return; }
-        if (longName == "Ancient Hearts") { shortName = QString("AH"); return; }
-        if (longName == "Lords of the Night") { shortName = QString("LotN"); return; }
+        if (longName == "Jyhad") { shortName = QString("Jyhad"); return; }
+        if (longName == "Vampire: The Eternal Struggle") { shortName = QString("V:TES"); return; }
         if (longName == "Dark Sovereigns") { shortName = QString("DS"); return; }
-        if (longName == "Vampire: The Eternal Struggle") { shortName = QString("VTES"); return; }
-        if (longName == "Anthology") { shortName = QString("Ath"); return; }
-        if (longName == "New Blood II") { shortName = QString("NB2"); return; }
-        if (longName == "Heirs to the Blood") { shortName = QString("HttB"); return; }
+        if (longName == "Ancient Hearts") { shortName = QString("AH"); return; }
+        if (longName == "Sabbat") { shortName = QString("Sabbat"); return; }
+        if (longName == "Sabbat War") { shortName = QString("SW"); return; }
+        if (longName == "Final Nights") { shortName = QString("FN"); return; }
+        if (longName == "Bloodlines") { shortName = QString("BL"); return; }
+        if (longName == "Camarilla Edition") { shortName = QString("CE"); return; }
+        if (longName == "Anarchs") { shortName = QString("Anarchs"); return; }
+        if (longName == "Black Hand") { shortName = QString("BH"); return; }
+        if (longName == "Gehenna") { shortName = QString("Gehenna"); return; }
+        if (longName == "Tenth Anniversary") { shortName = QString("Tenth"); return; }
         if (longName == "Kindred Most Wanted") { shortName = QString("KMW"); return; }
         if (longName == "Legacies of Blood") { shortName = QString("LoB"); return; }
-        if (longName == "Fifth Edition") { shortName = QString("V5"); return; }
-        if (longName == "The Unaligned") { shortName = QString("TU"); return; }
-        if (longName == "Twilight Rebellion") { shortName = QString("TB"); return; }
-        if (longName == "Camarilla Edition") { shortName = QString("CE"); return; }
-        if (longName == "Anarch Unbound") { shortName = QString("AU"); return; }
-        if (longName == "Bloodlines") { shortName = QString("B"); return; }
-        if (longName == "Echoes of Gehenna") { shortName = QString("EoG"); return; }
-        if (longName == "Sword of Caine") { shortName = QString("SoC"); return; }
-        if (longName == "Keepers of Tradition Reprint") { shortName = QString("KoTR"); return; }
-        if (longName == "Black Hand") { shortName = QString("BH"); return; }
         if (longName == "Nights of Reckoning") { shortName = QString("NoR"); return; }
-        if (longName == "Heirs to the Blood Reprint") { shortName = QString("HttBR"); return; }
-        if (longName == "Final Nights") { shortName = QString("FN"); return; }
-        if (longName == "Third Edition") { shortName = QString("3th"); return; }
-        if (longName == "Keepers of Tradition") { shortName = QString("KoT"); return; }
-        if (longName == "Sabbat") { shortName = QString("S"); return; }
-        if (longName == "Ebony Kingdom") { shortName = QString("EK"); return; }
-        if (longName == "First Blood") { shortName = QString("1e"); return; }
-        if (longName == "Tenth Anniversary") { shortName = QString("10th"); return; }
-        if (longName == "Lost Kindred") { shortName = QString("LK"); return; }
-        if (longName == "Twenty-Fifth Anniversary") { shortName = QString("25th"); return; }
-        if (longName == "Fifth Edition (Anarch)") { shortName = QString("V5A"); return; }
-        if (longName == "Jyhad") { shortName = QString("J"); return; }
-        if (longName == "Sabbat Preconstructed") { shortName = QString("SP"); return; }
-        if (longName == "Fifth Edition (Companion)") { shortName = QString("V5C"); return; } 
+        if (longName == "Third Edition") { shortName = QString("Third"); return; }
+        if (longName == "Sword of Caine") { shortName = QString("SoC"); return; }
+        if (longName == "Lords of the Night") { shortName = QString("LotN"); return; }
         if (longName == "Blood Shadowed Court") { shortName = QString("BSC"); return; }
+        if (longName == "Twilight Rebellion") { shortName = QString("TR"); return; }
+        if (longName == "Keepers of Tradition") { shortName = QString("KoT"); return; }
+        if (longName == "Ebony Kingdom") { shortName = QString("EK"); return; }
+        if (longName == "Heirs to the Blood") { shortName = QString("HttB"); return; }
+        if (longName == "Danse Macabre") { shortName = QString("DM");  return; }
+        if (longName == "The Unaligned") { shortName = QString("TA"); return; }
+        /* Note; Storyline Rewards is missing. */
+        if (longName == "Anarch Unbound") { shortName = QString("AU"); return; }
+        if (longName == "Lost Kindred") { shortName = QString("LK"); return; }
+        if (longName == "Sabbat Preconstructed") { shortName = QString("SP"); return; }
+        if (longName == "Fifth Edition") { shortName = QString("V5"); return; }
+
+        /* Todo; put these in the correct order.. */
+        if (longName == "Fifth Edition (Anarch)") { shortName = QString("V5A"); return; }
+        if (longName == "Shadows of Berlin") { shortName = QString("SoB"); return; }
+        if (longName == "New Blood") { shortName = QString("NB"); return; }
+        if (longName == "Fall of London") { shortName = QString("FoL"); return; }
+        if (longName == "Anthology") { shortName = QString("Ath"); return; }
+        if (longName == "New Blood II") { shortName = QString("NB2"); return; }
+        if (longName == "Echoes of Gehenna") { shortName = QString("EoG"); return; }
+        if (longName == "Keepers of Tradition Reprint") { shortName = QString("KoTR"); return; }
+        if (longName == "Heirs to the Blood Reprint") { shortName = QString("HttBR"); return; }
+        if (longName == "First Blood") { shortName = QString("1e"); return; }
+        if (longName == "Twenty-Fifth Anniversary") { shortName = QString("25th"); return; }
+        if (longName == "Fifth Edition (Companion)") { shortName = QString("V5C"); return; } 
         if (longName == "Print on Demand") { shortName = QString("POD"); return; }
-        shortName = QString("Pr");
+        shortName = QString("Promo");
     }
 
     int addCard(QVariant card)
